1ddad9b639ac5b34f0c2efe9f1514cbdc533a6a7
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-07-08 11:57:10 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('.start');
+const buttonStop = document.querySelector('.stop');
+const bodyColor = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const handlerStart = () => {
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+};
+
+function handlerStop() {
+  clearInterval(timerId);
+  console.log('Stop');
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
+
+buttonStart.addEventListener('click', handlerStart);
+buttonStop.addEventListener('click', handlerStop);
